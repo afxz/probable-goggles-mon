7f103748550001a9edf6bb7e6d3809b29d60e692
Nova <157980606+afxz@users.noreply.github.com>
2025-07-05 09:16:42 +0000

    asd

--- bot/api.py
+++ bot/api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from db import add_points
+from .db import add_points
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
